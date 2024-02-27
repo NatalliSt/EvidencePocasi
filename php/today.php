9358cec713fe43fb5565595dfe1228783e868e75
Natalie Starhova <136753226+NatalliSt@users.noreply.github.com>
2024-02-27 21:16:10 +0100

    Update today.php

--- php/today.php
+++ php/today.php
@@ -179,23 +179,23 @@ $wind = -1000;
 $rainfall = -1000;
 
 // Check each input field and update the corresponding variable if it's not empty
-if (!empty($_POST["tempIn"])) {
-$tempIn = $_POST["tempIn"];
+if (!empty($_POST["tempIn"]) || $_POST["tempIn"] === '0') {
+    $tempIn = $_POST["tempIn"];
 }
-if (!empty($_POST["tempOut"])) {
-$tempOut = $_POST["tempOut"];
+if (!empty($_POST["tempOut"]) || $_POST["tempOut"] === '0') {
+    $tempOut = $_POST["tempOut"]; 
 }
-if (!empty($_POST["pressure"])) {
-$pressure = $_POST["pressure"];
+if (!empty($_POST["pressure"]) || $_POST["pressure"] === '0') {
+    $pressure = $_POST["pressure"];
 }
-if (!empty($_POST["humidity"])) {
-$humidity = $_POST["humidity"];
+if (!empty($_POST["humidity"]) || $_POST["humidity"] === '0') {
+    $humidity = $_POST["humidity"];
 }
-if (!empty($_POST["wind"])) {
-$wind = $_POST["wind"];
+if (!empty($_POST["wind"]) || $_POST["wind"] === '0') {
+    $wind = $_POST["wind"];
 }
-if (!empty($_POST["rainfall"])) {
-$rainfall = $_POST["rainfall"];
+if (!empty($_POST["rainfall"]) || $_POST["rainfall"] === '0') {
+    $rainfall = $_POST["rainfall"];
 }
 
 $sql = "insert into data_ep(users_id, dateNumber, timeNumber, yearNumber, monthNumber, weekNumber, dayNumber, temp_in, temp_out, pressure, humidity, wind, rainfall)" .
